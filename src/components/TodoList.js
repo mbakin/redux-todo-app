24f87ee3e05b2769954bbd8021726857f8517376
mbakin <mehmetberkakinn@outlook.com>
2021-09-19 20:40:22 +0300

    Add toggle completed

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,28 +1,32 @@
-import React from 'react'
+import React from "react";
 
-import { useSelector } from 'react-redux'
-
+import { useSelector, useDispatch } from "react-redux";
+import { toggle } from "../redux/todos/todosSlice";
 
 const TodoList = () => {
-
-	const items = useSelector((state) => state.todos.items) // this key todos = todosSlice>name:'todos'
-
+  const dispatch = useDispatch();
+  const items = useSelector((state) => state.todos.items); // this key todos = todosSlice>name:'todos'
 
   return (
     <ul className="todo-list">
-			{
-				items.map((item) => (
-					<li key={item.id} className={item.completed ? 'completed' : ''}>
-				<div className="view">
-					<input className="toggle" type="checkbox"/>
-					<label>{item.title}</label>
-					<button className="destroy"></button>
-				</div>
-			</li>
-				))
-			}
-		</ul>
-  )
-}
+      {items.map((item) => (
+        <li key={item.id} className={item.completed ? "completed" : ""}>
+          <div className="view">
+            <input
+              className="toggle"
+              type="checkbox"
+							checked={item.completed}
+              onChange={() => {
+                dispatch(toggle({ id: item.id }));
+              }}
+            />
+            <label>{item.title}</label>
+            <button className="destroy"></button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
-export default TodoList
+export default TodoList;
