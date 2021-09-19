51e641ea9d5371a80438fd7029bac12c84eb814c
mbakin <mehmetberkakinn@outlook.com>
2021-09-19 21:01:19 +0300

    Add 'handleDestroy'  actions

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 import { useSelector, useDispatch } from "react-redux";
-import { toggle } from "../redux/todos/todosSlice";
+import { destroy, toggle } from "../redux/todos/todosSlice";
 
 const TodoList = () => {
   const dispatch = useDispatch();
   const items = useSelector((state) => state.todos.items); // this key todos = todosSlice>name:'todos'
+
+	const handleDestroy = (id, title) =>{
+		if(window.confirm(`Are you sure delete ${title} ?`)){
+			dispatch(destroy(id))
+		}
+	}
 
   return (
     <ul className="todo-list">
@@ -17,11 +23,11 @@ const TodoList = () => {
               type="checkbox"
 							checked={item.completed}
               onChange={() => {
-                dispatch(toggle({ id: item.id }));
-              }}
+								dispatch(toggle({ id: item.id }));
+							}}
             />
             <label>{item.title}</label>
-            <button className="destroy"></button>
+            <button className="destroy" onClick={() =>handleDestroy(item.id, item.title)}></button>
           </div>
         </li>
       ))}
